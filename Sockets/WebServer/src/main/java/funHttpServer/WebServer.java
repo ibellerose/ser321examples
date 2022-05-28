a34f563ec16cc1ce6e1b3892eaee83788fae718e
ibellerose <ianbellerose@gmail.com>
2022-05-28 12:26:51 -0400

    Edit catch message

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -218,7 +218,7 @@ class WebServer {
             builder.append("HTTP/1.1 400 Bad Request\n");
             builder.append("Content-Type: text/html; charset=utf-8\n");
             builder.append("\n");
-            builder.append("The correct format is multiply?num1=2&num2=4");
+            builder.append("400 Bad Request!:The correct format is '/multiply?num1=2&num2=4'");
           }
           // Generate response
 
