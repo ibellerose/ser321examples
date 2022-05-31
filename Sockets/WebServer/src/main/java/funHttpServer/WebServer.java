5efb0825fe1cf17c7124593837091df06d721a66
ibellerose <ianbellerose@gmail.com>
2022-05-31 17:03:50 -0400

    attempt to do spaces

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -360,11 +360,11 @@ class WebServer {
           for(int i = 1; i <= rows; ++i){
             k = 0;
             for(int j = 1; j<= rows - i; ++j){
-              pyramid += "   ";
+              pyramid += "&nbsp&nbsp  ";
               System.out.print("  ");
             }
             while(k != 2 * i - 1){
-              pyramid += block + " ";
+              pyramid += block + "&nbsp ";
               System.out.print(block + " ");
               k++;
             }
