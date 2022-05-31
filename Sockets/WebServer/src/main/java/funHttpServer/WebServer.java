2e30a006377ee957ddb79471f126a0b16890ac08
ibellerose <ianbellerose@gmail.com>
2022-05-31 17:32:46 -0400

    fix error of rows paramater

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -359,7 +359,7 @@ class WebServer {
             builder.append("\n");
             builder.append("The first parameter name must be 'block'");
           } 
-          else if(!request.contains("?rows=")){
+          else if(!request.contains("&rows=")){
             builder.append("HTTP/1.1 400 Bad Request\n");
             builder.append("Content-Type: text/html; charset=utf-8\n");
             builder.append("\n");
