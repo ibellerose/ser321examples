4e79a4c017c0e32ae6af0c5641617f36b37b57c6
ibellerose <ianbellerose@gmail.com>
2022-05-31 17:32:05 -0400

    change error message of block error

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -381,7 +381,7 @@ class WebServer {
               builder.append("HTTP/1.1 400 Bad Request\n");
               builder.append("Content-Type: text/html; charset=utf-8\n");
               builder.append("\n");
-              builder.append("The second parameter name must be 'rows'");
+              builder.append("The value of block must be equal to a single character");
             }
             else{
               try{
