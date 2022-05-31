749ba450149b76e4c33ab77a6da5592ebbde8caa
ibellerose <ianbellerose@gmail.com>
2022-05-31 16:44:22 -0400

    fix look of pyramid

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -362,7 +362,7 @@ class WebServer {
             k = 0;
             for(int j = 1; j<= rows - i; ++j){
               pyramid += "   ";
-              System.out.print("   ");
+              System.out.print("  ");
             }
             while(k != 2 * i - 1){
               pyramid += "b" + " ";
